2d829931468f6bc65a0ff0a51f6714de69dea2f6
lun_hao <lun_hao@123cx.com>
2018-05-15 11:04:23 +0800

    backup

--- app.js
+++ app.js
@@ -2,23 +2,54 @@
 const weixinStrategy = require("passport-weixin");
 
 module.exports = app => {
-  var config = app.config.passportWeiXin;
-  config.passReqToCallback = true;
-  config.clientSecret = config.secret;
-  var client = "snsapi_login";
-  if (config.scope == "snsapi_userinfo") {
-    config.authorizationURL =
-      "https://open.weixin.qq.com/connect/oauth2/authorize";
-    client = "loginByWeixinClient";
-  }
+  var config = app.config.passportAsiczWeixin;
+  //   config.passReqToCallback = true;
+  //   config.clientSecret = config.secret;
+  //   var client = "snsapi_login";
+  //   if (config.scope == "snsapi_userinfo") {
+  //     config.authorizationURL =
+  //       "https://open.weixin.qq.com/connect/oauth2/authorize";
+  //     client = "loginByWeixinClient";
+  //   }
 
-  config.requireState = false;
-  app.logger.info(config);
+  //   config.requireState = false;
+  //   app.logger.info(config);
+  //   app.passport.use(
+  //     client,
+  //     new weixinStrategy(
+  //       config,
+  //       (req, accessToken, refreshToken, profile, done) => {
+  //         const user = {
+  //           provider: "weixin",
+  //           id: profile.id,
+  //           displayName: profile.displayName,
+  //           photo: profile.profileUrl,
+  //           emails: profile.emails,
+  //           accessToken: accessToken,
+  //           refreshToken: refreshToken
+  //         };
+
+  //         //debug('%s %s get user: %j', req.method, req.url, user);
+  //         // app.logger.info(user);
+  //         app.passport.doVerify(req, user, done);
+  //       }
+  //     )
+  //   );
+
+  //扫码登录
+  //微信官网文档：https://open.weixin.qq.com/cgi-bin/showdocument?action=dir_list&t=resource/res_list&verify=1&id=open1419316505&token=&lang=zh_CN
   app.passport.use(
-    client,
-    new weixinStrategy(
-      config,
-      (req, accessToken, refreshToken, profile, done) => {
+    "loginByWeixin",
+    new WeixinStrategy(
+      {
+        clientID: config.clientID,
+        clientSecret: config.clientSecret,
+        callbackURL: config.callbackURL,
+        requireState: false,
+        scope: "snsapi_login"
+      },
+      (accessToken, refreshToken, profile, done) => {
+        app.logger.info(profile);
         const user = {
           provider: "weixin",
           id: profile.id,
@@ -28,9 +59,35 @@ module.exports = app => {
           accessToken: accessToken,
           refreshToken: refreshToken
         };
+        app.passport.doVerify(req, user, done);
+      }
+    )
+  );
 
-        //debug('%s %s get user: %j', req.method, req.url, user);
-        //app.logger.info(user);
+  //微信客户端登录
+  //微信官网文档：http://mp.weixin.qq.com/wiki/17/c0f37d5704f0b64713d5d2c37b468d75.html
+  passport.use(
+    "loginByWeixinClient",
+    new WeixinStrategy(
+      {
+        clientID: config.clientID,
+        clientSecret: config.clientSecret,
+        callbackURL: config.callbackURL,
+        requireState: false,
+        authorizationURL: "https://open.weixin.qq.com/connect/oauth2/authorize", //[公众平台-网页授权获取用户基本信息]的授权URL 不同于[开放平台-网站应用微信登录]的授权URL
+        scope: "snsapi_userinfo" //[公众平台-网页授权获取用户基本信息]的应用授权作用域 不同于[开放平台-网站应用微信登录]的授权URL
+      },
+      (accessToken, refreshToken, profile, done) => {
+        app.logger.info(profile);
+        const user = {
+          provider: "weixin",
+          id: profile.id,
+          displayName: profile.displayName,
+          photo: profile.profileUrl,
+          emails: profile.emails,
+          accessToken: accessToken,
+          refreshToken: refreshToken
+        };
         app.passport.doVerify(req, user, done);
       }
     )
